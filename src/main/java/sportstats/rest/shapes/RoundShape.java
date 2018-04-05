40b180ebd58d38626640887bcbfb8fdaf81948ee
Erijoda <erijoda1991@gmail.com>
2018-04-05 12:36:19 +0200

    Moved shapes from sparkproject to backend.

---
+++ src/main/java/sportstats/rest/shapes/RoundShape.java
@@ -0,0 +1,17 @@
+
+package sportstats.rest.shapes;
+
+import java.util.List;
+
+/**
+ *
+ * @author mattkranc
+ */
+public class RoundShape {
+    public Long seasonId;
+    public List<GameShape> games;
+    
+    public RoundShape(){
+        
+    }
+}
