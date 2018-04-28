b75f8a5c6cf973df243382666f63f9411a2eab28
Rebecca <Rebecca@130.243.9.190>
2018-04-28 14:16:46 +0200

    round interval med test

---
+++ src/test/java/sportstats/service/tables/GetTableByRoundIntervalIT.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sportstats.service.tables;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import sportstats.service.tables.GetTableByRoundInterval;
+
+/**
+ *
+ * @author Rebecca
+ */
+public class GetTableByRoundIntervalIT {
+    
+    public GetTableByRoundIntervalIT() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        System.out.println(new GetTableByRoundInterval(1L, 8L, 10L));
+    }
+    
+}
